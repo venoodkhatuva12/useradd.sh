41fd7c761dc0ecfc873bb0125953c3809c28b247
Venood Katuwa <venood.khatuva12@gmail.com>
2016-08-08 13:04:32 +0530

    Update and rename LICENSE.md to script.sh

---
+++ script.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+read -p "Please Enter the Username : " user
+useradd $user
+passwd $user
+su $user
+ssh-keygen -t rsa -f $user
+mkdir .ssh
+cat $user.pub > .ssh/authorized_keys
+chmod -R 700 .ssh/
+chown -R $user:$user .ssh/
+mv $user $user.pem
+pkill -KILL -u $user
+read -p "Do you want to add this User to Sudoer(Yes/No)? : " response
+if [[ $response =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+   sudo sed -i "95 i $user   ALL=(ALL)       ALL" /etc/sudoers
+else
+    exit
+fi
